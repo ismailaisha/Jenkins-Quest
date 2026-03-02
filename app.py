9d5a511bb5b7a63f875f04d22c63955e8f18c164
Aishan <tabladance@mail.ru>
2026-03-02 20:47:16 +0400

    Измененение ффайлов

--- app.py
+++ app.py
@@ -16,17 +16,3 @@ def report(value):
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
 
-test_app.py
-from app import app
-
-def test_health():
-    client = app.test_client()
-    response = client.get("/health")
-    assert response.status_code == 200
-    assert response.json["status"] == "ok"
-
-def test_report():
-    client = app.test_client()
-    response = client.get("/report/5")
-    assert response.status_code == 200
-    assert response.json["result"] == 50
